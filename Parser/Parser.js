e6250bba63cd0245a733acebf4c15af1fd3b1a7b
trojan565 <majorexo565@gmail.com>
2022-11-19 21:20:27 +0300

    HTML Parser

---
+++ Parser/Parser.js
@@ -0,0 +1,12 @@
+const HTMLParser=(html,to)=>{
+    let parser=new DOMParser();
+    parser=parser.parseFromString(html,"text/html");
+    if(to){
+        document.querySelectorAll(to).forEach((element)=>{
+            element.textContent="";
+            element.append(parser.body.childNodes[0])
+        })
+    }else{
+        return parser.body.childNodes[0];
+    }
+}
