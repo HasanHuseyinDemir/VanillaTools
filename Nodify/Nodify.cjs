1e9d92b380662cc689c2cc16b8e4b1cd25500b65
trojan565 <majorexo565@gmail.com>
2022-11-20 11:42:05 +0300

    cjs

---
+++ Nodify/Nodify.cjs
@@ -0,0 +1,49 @@
+let nodes=[];
+
+const nodeValueEqualizer=()=>{
+    if(nodes.length){
+        nodes.forEach((node)=>{
+            let values=(new Function("return "+node.value)());
+            node.node.nodeValue!=values?node.node.nodeValue=values:""
+        })
+    }
+}
+
+const createTextNode=(to,value,id)=>{
+    document.querySelectorAll(to).forEach((item)=>{
+        const countNode=document.createTextNode((new Function("return "+value)()));
+        nodes.push({node:countNode,value,id:id})
+        item.appendChild(countNode)
+    })
+}
+
+const destroyNodes=(id,deletes)=>{
+    let deletedNodes=nodes.filter((node)=>{
+        return node.id==id
+    })
+    if(deletes){
+        deletedNodes.forEach((node)=>{
+            node.node.nodeValue=""
+        })
+    }
+    nodes=nodes.filter((node)=>{
+        return node.id!=id
+    })
+}
+
+const convertToTextNode=(item,value)=>{
+    document.body.querySelectorAll(item).forEach((items)=>{
+        let dataVal=items.getAttribute("data");
+        const countNode=document.createTextNode((new Function("return "+value?value:dataVal)()));
+        nodes.push({node:countNode,value:value?value:dataVal,id:item})
+        items.parentNode.replaceChild(countNode,items);
+    })
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    document.body.addEventListener("click",()=>{nodeValueEqualizer()})
+    nodeValueEqualizer();
+})
+
+
+export {nodes,nodeValueEqualizer,createTextNode,destroyNodes,convertToTextNode}
