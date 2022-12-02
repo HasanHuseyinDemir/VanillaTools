6475c20fc7dc5c1baa9aef3a49ee98669768f895
trojan565 <majorexo565@gmail.com>
2022-12-02 15:44:33 +0300

    simple math calculations avg min max log ...etc

---
+++ Utility/math/index.mjs
@@ -0,0 +1,9 @@
+import * as prototypes from "./math.mjs"
+
+let a=[1,2,3,4,10];
+
+//.log is getter of console.log(this)
+
+a.type.log;
+a.max.log;
+a.average.log;
---
+++ Utility/math/math.mjs
@@ -0,0 +1,39 @@
+Object.defineProperty(Array.prototype,"sum",{
+    get:function(){
+        return this.reduce((arg,acc)=>{return arg+acc});
+    }
+})
+
+Object.defineProperty(Array.prototype,"average",{
+    get:function(){
+        return this.reduce((e,i)=>e+i)/this.length
+    }
+})
+
+Object.defineProperty(Array.prototype,"min",{
+    get:function(){
+        let arr=this.sort((e,i)=>{
+            return e-i
+        })
+        return arr[0];
+    }
+})
+
+Object.defineProperty(Array.prototype,"max",{
+    get:function(){
+        let arr=this.sort((e,i)=>{
+        return e-i
+    })
+    return arr[this.length-1]
+    }
+})
+
+Object.defineProperty(Object.prototype,"log",{
+    get:function(){
+        console.log(this);
+    }
+})
+
+Object.defineProperty(Object.prototype,"type",{
+    get:function(){return typeof this}
+})
